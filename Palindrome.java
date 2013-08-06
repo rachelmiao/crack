c750b3374c5aabc9eaf06780fe98817dd2b47b52
j2pierce <j2pierce@gmail.com>
2013-08-05 20:58:18 -0500

    modified Palindrome to properly reverse

--- Palindrome.java
+++ Palindrome.java
@@ -1,7 +1,6 @@
-
 /**
- * Write a method to check if a string is a palindrome
- */
+* Write a method to check if a string is a palindrome
+*/
 public class Test {
 	public static void main(String[] args) {
 		String testString = "hellolleh";
@@ -14,13 +13,24 @@ public class Test {
 		return (rev.equals(s));
 	}
 
-	static String reverse(String s) {
+//	static String reverse(String s) {
+//		char[] arr = s.toCharArray();
+//		for (int i = 0; i < arr.length; i++) {
+//			char temp = arr[i];
+//			arr[i] = arr[arr.length - i - 1];
+//			arr[arr.length - i - 1] = temp;
+//		}
+//	return String.copyValueOf(arr);
+//	}
+	
+	// The previous method did not properly reverse the string.
+	// This should do the trick! :)
+	static String reverse(String s){
 		char[] arr = s.toCharArray();
-		for (int i = 0; i < arr.length; i++) {
-			char temp = arr[i];
-			arr[i] = arr[arr.length - i - 1];
-			arr[arr.length - i - 1] = temp;
+		char[] toreturn = new char[arr.length];
+		for (int i = 0; i < arr.length; i++){
+			toreturn[i] = arr[arr.length - (i+1)];
 		}
-		return String.copyValueOf(arr);
+		return String.copyValueOf(toreturn);
 	}
 }
